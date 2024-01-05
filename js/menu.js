28a249b3d5f722a2166cd31003e47ba3aaf6c68e
Caio <108128119+DocCaio@users.noreply.github.com>
2024-01-05 13:35:54 -0300

    up

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".list-nav");
+
+hamburger.addEventListener("click", () =>{
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".list-nav-link").forEach(n => n.addEventListener("click" , () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
+
+

6cc5f78102d58cacdd3ec22a9208a1f21fce13a0
Caio <108128119+DocCaio@users.noreply.github.com>
2024-01-08 13:33:56 -0300

    up

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,28 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".list-nav");
+//const hamburger = document.querySelector(".hamburger");
+//const navMenu = document.querySelector(".list-nav");
 
-hamburger.addEventListener("click", () =>{
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
+//hamburger.addEventListener("click", () =>{
+ //hamburger.classList.toggle("active");
+ // navMenu.classList.toggle("active");
+//});
 
-document.querySelectorAll(".list-nav-li").forEach(n => n.addEventListener("click" , () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}));
+//document.querySelectorAll(".list-nav-li").forEach(n => n.addEventListener("click" , () => {
+//  hamburger.classList.remove("active");
+//  navMenu.classList.remove("active");
+//}));
+
+
+const button = document.querySelector('.menu__button')
+
+button.addEventListener('click', funcaoCallback)
+
+function funcaoCallback() {
+  const menuNav = document.querySelector('.nav')
+  menuNav.classList.toggle('menu-active')
+  
+  button.textContent === "x"
+    ? button.textContent = "X"
+    : button.textContent = "x"
+}
 
 
